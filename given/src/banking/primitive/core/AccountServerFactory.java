82886883a32b3344ab2edd4472442eb8dbdb2fc6
blebidju <blebidju@asu.edu>
2017-02-20 21:32:28 -0700

    Add files via upload
    Added file banner for account server factory java file

--- given/src/banking/primitive/core/AccountServerFactory.java
+++ given/src/banking/primitive/core/AccountServerFactory.java
@@ -1,6 +1,12 @@
 package banking.primitive.core;
 
+/*
+File: AccountServerFactory.java
+Author:	Brandon Lebid-Jurtschyk
+Date: 2/19/17
 
+Description: Returns an Account Server Factory if get me is called otherwise it returns a ServerSolution if Lookup is called
+*/
 public class AccountServerFactory {
 
 	protected static AccountServerFactory singleton = null;
@@ -8,15 +14,26 @@ public class AccountServerFactory {
 	protected AccountServerFactory() {
 
 	}
-
+	
+//	/**
+//	  Method:getMe
+//	  Inputs:N/A
+//	  Returns:Singleton
+//
+//	  Description:If singleton is null it will initialize it with an object made of this class otherwise if it 
+//	  is already a member then it will throw back singleton as it was
+//	*/
 	public static AccountServerFactory getMe() {
-		if (singleton == null) {
-			singleton = new AccountServerFactory();
-		}
-
-		return singleton;
+		return new AccountServerFactory();
 	}
 
+	/**
+	  Method:lookup
+	  Inputs:N/A
+	  Returns:ServerSolution
+
+	  Description:Returns a base initialized ServerSolution object
+	*/
 	public AccountServer lookup() {
 		return new ServerSolution();
 	}
